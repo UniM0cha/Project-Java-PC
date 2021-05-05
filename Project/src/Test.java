8052a40578890cf30571a5e7577099c9ab89c8fa
Hanav52 <wnsrl8329@gmail.com>
2021-05-05 20:02:22 +0900

    정우교육

--- Project/src/Test.java
+++ Project/src/Test.java
@@ -1,8 +1,17 @@
+import javax.swing.JFrame;
 
-public class Test {
+public class Test extends JFrame{
+
+	public Test(String title, int width, int height) {
+		setTitle(title);
+		setSize(width, height);
+		setLocationRelativeTo(this);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("준기 귀여워");
+		new Test("정우 똥꼬 핑크 똥꼬", 300, 300);
 	}
 
 }
